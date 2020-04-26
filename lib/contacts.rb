5aa93994fee6a1bc6918f7fa7d0e649ddc076a7f
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-26 20:29:27 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,9 +13,9 @@ require 'pry'
       favorite_ice_cream_flavors: ["cookie dough", "mint chip"]
     }
   }
-#
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
+    binding.pry
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
